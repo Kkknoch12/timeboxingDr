d1df6f0baee6b8eecef49e12d3e52e0005db1e70
diego chavez <23393292@utcancun.edu.mx>
2024-11-26 19:14:55 -0500

    confirmar

--- prisma/migrations/20241120134119_change_priority_default/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `priority` on the `task` table. The data in that column could be lost. The data in that column will be cast from `Enum(EnumId(0))` to `Enum(EnumId(0))`.
-
-*/
--- AlterTable
-ALTER TABLE `task` MODIFY `priority` ENUM('HIGH', 'MEDIUM', 'LOW') NOT NULL DEFAULT 'LOW';
